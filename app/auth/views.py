007382ff03acce093ec36c790f7c3b090ed72f49
[muneneee] <[lukekevin39@gmail.com]>
2020-05-03 16:03:30 +0300

    added a logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@ from flask import render_template,redirect,url_for,flash,request
 from ..models import User
 from .forms import RegistrationForm, LoginForm
 from . import auth
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 
 
 
@@ -31,3 +31,10 @@ def register():
         db.session.add(user)
         db.session.commit()
     return render_template('auth/register.html',registration_form = form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
