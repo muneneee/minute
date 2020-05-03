1acdf20d0c2344cd66674e1a2ec88fa63ed5197f
[muneneee] <[lukekevin39@gmail.com]>
2020-05-03 20:38:54 +0300

    added the pitch html

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,6 +3,17 @@ from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 
 
+
+
+class ReviewForm(FlaskForm):
+
+
+    title = StringField('Pitch title',validators=[Required()])
+    Pitch = TextAreaField('Pitch', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators =[Required()])
     submit = SubmitField('Submit')
