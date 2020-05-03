f5c05aaad8581632c1f47b470fc049b039c84d24
[muneneee] <[lukekevin39@gmail.com]>
2020-05-03 18:04:28 +0300

    added edit profile page

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,8 @@
-from flask import render_template
+from flask import render_template, redirect,url_for, abort
 from . import main
+from .forms import UpdateProfile
+from flask_login import login_required
+from .. import db
 
 
 @main.route('/')
@@ -17,3 +20,23 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_onsubmit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form = form)
