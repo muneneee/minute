83735d77f8f0fb2a8c4ac5ee1e246c4da2439a03
[muneneee] <[lukekevin39@gmail.com]>
2020-05-03 19:21:01 +0300

    created the email templates

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,6 +3,7 @@ from . import auth
 from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import LoginForm,RegistrationForm
+from ..email import mail_message
 from .. import db
 
 @auth.route('/login',methods=['GET','POST'])
@@ -33,6 +34,9 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to Pitcher","email/welcome_user",user.email,user = user)
+
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
