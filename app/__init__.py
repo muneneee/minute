da5496960e5ffd48e988145c34df492746561d91
[muneneee] <[lukekevin39@gmail.com]>
2020-05-03 15:04:37 +0300

    installed flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import Config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask-login import LoginManager
 import os
 
 
@@ -23,10 +24,14 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
 
      # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     return app
